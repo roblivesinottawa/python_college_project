1c87d2d6ef552fb9c819eddd0aa5dd6cf3d27c87
Robson Ferreira <tech.rob@icloud.com>
2022-12-23 13:48:07 -0500

    Create test.py

---
+++ notebooks/test.py
@@ -0,0 +1,26 @@
+# x = 0
+# y = 0
+# z = 0
+
+# y = int(input("type a number: "))
+# print(y)
+
+# z = y * 3
+# x = z - 9
+# if x < 0:
+#     print("negative number")
+# elif x > 0:
+#     print("positive number")
+# else:
+#     print("zero")
+
+a = 9
+b = 10
+c = 8
+d = 11
+f = 10
+
+print(a > c)
+print(b <= f)
+print((a > b) and (c > d))
+print(b == f)
